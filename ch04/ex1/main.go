cd4851e70f3b2d2c3897e2d872e8055155899042
Luciano Soares <soaresluciano@gmail.com>
2023-10-25 10:18:54 +0000

    add ch4 ex1

--- ch04/ex1/main.go
+++ ch04/ex1/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+)
 
+func main() {
+	max := 100
+	s := make([]int, max)
+
+	for i := 0; i < max; i++ {
+		value := rand.Intn(max)
+		s[i] = value
+		fmt.Println(i, value)
+	}
 }
