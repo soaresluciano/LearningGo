f862eef245f1be6e6f62b823041f083b7db0c32a
Luciano Soares <soaresluciano@gmail.com>
2023-10-25 10:36:10 +0000

    add ch4 ex 2

--- ch04/ex2/main.go
+++ ch04/ex2/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+)
 
+func main() {
+	max := 100
+	s := make([]int, max)
+
+	for i := 0; i < max; i++ {
+		value := rand.Intn(max)
+		s[i] = value
+
+		PrintMessage(i, value)
+	}
+}
+
+func PrintMessage(i int, v int) {
+	divBy2 := v%2 == 0
+	divBy3 := v%3 == 0
+	both := divBy2 && divBy3
+
+	var message string
+
+	switch {
+	case both:
+		message = "Six!"
+	case divBy3:
+		message = "Three!"
+	case divBy2:
+		message = "Two!"
+	default:
+		message = "Never mind"
+	}
+
+	fmt.Println(i, v, message)
 }
