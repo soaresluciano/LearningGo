b68580ad22f74f38358da1a38e2a0a4a8b74571d
Luciano Soares <soaresluciano@gmail.com>
2023-11-01 11:23:46 +0000

    add ch5 ex3

--- ch05/ex3/main.go
+++ ch05/ex3/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	helloPrefix := prefixer("Hello")
+	fmt.Println(helloPrefix("Bob"))   // should print Hello Bob
+	fmt.Println(helloPrefix("Maria")) // should print Hello Maria
+}
+
+func prefixer(prefix string) func(string) string {
+	return func(body string) string {
+		return prefix + " " + body
+	}
 }
