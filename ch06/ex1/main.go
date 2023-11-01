81fee1e022f3dbcff4410e84cd43a4fcb1b84e71
Luciano Soares <soaresluciano@gmail.com>
2023-11-01 11:39:26 +0000

    add ch06 ex1

--- ch06/ex1/main.go
+++ ch06/ex1/main.go
@@ -1,5 +1,20 @@
 package main
 
 func main() {
+	MakePerson("John", "Doe", 32)
+	MakePersonPointer("Ann", "Moe", 24)
+}
 
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func MakePerson(firstName string, lastName string, age int) Person {
+	return Person{firstName, lastName, age}
+}
+
+func MakePersonPointer(firstName string, lastName string, age int) *Person {
+	return &Person{firstName, lastName, age}
 }
